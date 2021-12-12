47c32e39342dc5ff4c6e1e13d830b51117402549
Marwan Keshik <91651940+princemarwancash@users.noreply.github.com>
2021-12-12 07:57:30 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,18 @@
 #include <iostream>
-
-int main(){
-  return 0;
+#include "lib.h"
+using namespace std;
+int main() {
+    char list [10][20];
+    char name [20];
+    int a=0;
+    for(int i=0;i<10;i++){
+        cin>>list[i];
+    }
+    cin>>name;
+    if (res(list,name,a)){
+        cout <<a;
+    }else{
+        cout << "non presente";
+    }
+    return 0;
 }
