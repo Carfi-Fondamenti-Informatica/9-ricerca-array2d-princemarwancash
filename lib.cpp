f718a75c81aa9c3cc79300951b15dd6fec8e2f4b
Marwan Keshik <91651940+princemarwancash@users.noreply.github.com>
2021-12-12 07:56:29 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,13 @@
+#include "lib.h"
+#include <string.h>
+bool res (char list[10][20],char name[20],int &i){
+    for(i=0;i<10;i++){
+        if((strcmp(list[i],name))==0){
+            return true;
+        }
 
+
+    }
+    return false;
+
+}
